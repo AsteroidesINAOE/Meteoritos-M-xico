fcf1d44c9816e8ef4ebd7c1f34e21c45c71a739c
AsteroidesINAOE <104033354+AsteroidesINAOE@users.noreply.github.com>
2022-06-26 02:39:51 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,17 @@
+         var modal1 = document.getElementById("myModal1");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img1 = document.getElementById("ia1");
+        var modalImg1 = document.getElementById("img1");
+        img1.onclick = function(){
+        modal1.style.display = "block";
+        modalImg1.src = this.src;
+        }
+
+        // Get the <span> element that closes the modal
+        var span1 = document.getElementsByClassName("close1")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span1.onclick = function() { 
+          modal1.style.display = "none";
+        }
