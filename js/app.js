267ccc895b1a5369192c2c0362070bd37bc23402
AsteroidesINAOE <104033354+AsteroidesINAOE@users.noreply.github.com>
2022-06-26 02:43:55 -0500

    update

--- js/app.js
+++ js/app.js
@@ -22,10 +22,8 @@
         modal2.style.display = "block";
         modalImg2.src = this.src;
         }
-
         // Get the <span> element that closes the modal
         var span2 = document.getElementsByClassName("close2")[0];
-
         // When the user clicks on <span> (x), close the modal
         span2.onclick = function() { 
         modal2.style.display = "none";
